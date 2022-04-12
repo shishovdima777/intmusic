c73b0de44ba6bc4de5120171a2368b2cc4e21521
shishovdima777 <100834036+shishovdima777@users.noreply.github.com>
2022-04-12 18:50:56 +0300

    made singin/up pages

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../scss/login.scss";
 import { logoblack } from "../assets/images";
+import { Link } from "react-router-dom";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import AppleIcon from "@mui/icons-material/Apple";
 import GoogleIcon from "@mui/icons-material/Google";
@@ -8,47 +9,48 @@ import GoogleIcon from "@mui/icons-material/Google";
 function LogIn() {
   return (
     <div className="login-wrapper">
-      <img className="login-img" src={logoblack} alt="logoblack" />
+      <Link to="/">
+        <img className="login-img" src={logoblack} alt="logoblack" />
+      </Link>
       <span className="login-horizontal-line"></span>
       <p>Sign in to Intmusic to continue.</p>
       <div className="api-logins">
-        <div className="api-logins__elem1">
-        <a href="#">
-          <FacebookIcon className="icon-size" />
+        <a href="#" className="api-logins__elem1">
+          <FacebookIcon className="icon-size" /> LOGIN WITH FACEBOOK
         </a>
-        <a href="#">LOGIN WITH FACEBOOK</a>
-        </div>
-        <div className="api-logins__elem2">
-        <a href="#">
-          <AppleIcon className="icon-size" />
+        <a href="#" className="api-logins__elem2">
+          <AppleIcon className="icon-size" /> SIGN IN WITH APPLE
         </a>
-        <a href="#">SIGN IN WITH APPLE</a>
-        </div>
-        <div className="api-logins__elem3">
-        <a href="#">
-          <GoogleIcon className="icon-size" />
+        <a href="#" className="api-logins__elem3">
+          <GoogleIcon className="icon-size" /> LOGIN IN WITH GOOGLE
         </a>
-        <a href="#">LOGIN IN WITH GOOGLE</a>
-        </div>
       </div>
       <div className="login-or">
-        <span className="login-horizontal-line-2"></span>
+        <div className="login-horizontal-line-2"></div>
         <p>OR</p>
-        <span className="login-horizontal-line-2"></span>
+        <div className="login-horizontal-line-2"></div>
       </div>
-      <form action="#">
+      <form action="#" className="login-form">
         <label htmlFor="#">Email or username</label>
-        <input type="email" />
+        <input type="email" className="form-control form-control-lg" />
         <label htmlFor="#">Password</label>
-        <input type="password" />
+        <input type="password" className="form-control form-control-lg" />
         <p>Forgot your password?</p>
-        <label htmlFor="#">Remember me</label>
-        <input type="checkbox" />
-        <input type="submit" value="submit" />
+        <div className="login-remember-submit">
+          <div>
+            <input type="checkbox" className="check-inline" />
+            <label htmlFor="#" className="remember-me">
+              Remember me
+            </label>
+          </div>
+          <input type="submit" value="TO COME IN" className="to-come-in" />
+        </div>
       </form>
-      <span></span>
-      <p>Don't have an account?</p>
-      <a href="#">SIGNING UP FOR INTMUSIC</a>
+      <span className="login-horizontal-line-2"></span>
+      <p id="no-account">Don't have an account?</p>
+      <Link to="/signup" className="sign-up-link">
+        SIGNING UP FOR INTMUSIC
+      </Link>
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,122 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { logoblack } from "../assets/images";
+import "../scss/signup.scss";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
 
 function SignUp() {
   return (
-    <div>
-      <h1>I'm Sign Up page!</h1>
+    <div className="signup-wrapper">
+      <Link to="/">
+      <img src={logoblack} alt="logo" />
+      </Link>
+      <h1>Register and listen for free</h1>
+      <div className="api-signups">
+        <a href="#" className="api-signups__elem1">
+          <FacebookIcon className="icon-size" /> Register with Facebook
+        </a>
+        <a href="#" className="api-signups__elem2">
+          <GoogleIcon className="icon-size" /> Register with Google
+        </a>
+      </div>
+      <div className="signup-or">
+        <div className="signup-horizontal-line"></div>
+        <p>or</p>
+        <div className="signup-horizontal-line"></div>
+      </div>
+      <p className="second-p">Enter your email and password</p>
+      <form className="signup-form">
+        <label htmlFor="#">Your e-mail address</label>
+        <input
+          type="email"
+          className="form-control form-control-lg"
+          id="exampleFormControlInput1"
+          placeholder="Enter your email address"
+        />
+        <label htmlFor="#">Confirm email address</label>
+        <input
+          type="email"
+          className="form-control form-control-lg"
+          placeholder="Enter your email and password"
+        />
+        <label htmlFor="#">Pick a password</label>
+        <input
+          type="password"
+          className="form-control form-control-lg"
+          placeholder="Pick a password"
+        />
+        <label htmlFor="#">Your name</label>
+        <input
+          type="text"
+          className="form-control form-control-lg name-input"
+          placeholder="Enter your name"
+        />
+        <p>It will appear on your profile.</p>
+        <label htmlFor="#">Your date of birth</label>
+        <div className="signup-birth-date">
+          <div>
+            <label htmlFor="#">Day</label>
+            <input
+              type="text"
+              className="form-control form-control-lg birth-date__elem1"
+              placeholder="DD"
+            />
+          </div>
+          <div>
+            <label htmlFor="#">Month</label>
+            <select
+              className="form-select form-select-lg birth-date__elem2"
+              aria-label="Default select example"
+            >
+              <option selected className="birth-date__elem2">
+                Month
+              </option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="#">Year</label>
+            <input
+              type="text"
+              className="form-control form-control-lg birth-date__elem3"
+              placeholder="YYYY"
+            />
+          </div>
+        </div>
+        <label htmlFor="#">Please enter your gender</label>
+        <div className="gender-radio">
+          <div className="gender-radio-elem1">
+            <input type="radio" />
+            <label htmlFor="#">Man</label>
+          </div>
+          <div className="gender-radio-elem2">
+            <input type="radio" />
+            <label htmlFor="#">Female</label>
+          </div>
+        </div>
+        <div className="terms-checkboxes">
+          <div className="terms-checkboxes__elem1">
+        <input type="checkbox" />
+        <label htmlFor="#">
+          I want to receive news and offers from Intmusic.
+        </label>
+        </div>
+        <div className="terms-checkboxes__elem2">
+        <input type="checkbox" />
+        <label htmlFor="#">
+          I accept Intmusic's <a href="#">Terms of Service</a> and{" "}
+          <a href="#">Privacy Policy</a>.
+        </label>
+        </div>
+        </div>
+        <input type="submit" value="Register" className="sign-up-button"/>
+      </form>
+      <p className="login-link">
+        Already have an account? <Link to="/login">Login</Link>.
+      </p>
     </div>
   );
 }
